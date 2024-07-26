3628dcfededcccd9c82dae25f12d93960797ebb8
stanisraja <stanis.raja@gmail.com>
2024-07-26 08:47:20 -0400

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
 import Quiz from './Components/Quiz/Quiz'
 
 
 const App = () => {
   return (
-    <>
-      <Quiz/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+      </Routes>
+    </Router>
   )
 }
 
