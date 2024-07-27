07de433030339de5ada77f6630af915e71d404b1
stanisraja <stanis.raja@gmail.com>
2024-07-27 07:54:53 -0400

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Components/Home/Home';
+import Homepage from './Components/Home/Home';
 import Quiz from './Components/Quiz/Quiz'
 
 
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/quiz" element={<Quiz />} />
       </Routes>
     </Router>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App'; // Ensure the path to your App component is correct
-import './Components/Quiz/Quiz.css'
-
+import './Components/Quiz/Quiz.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
